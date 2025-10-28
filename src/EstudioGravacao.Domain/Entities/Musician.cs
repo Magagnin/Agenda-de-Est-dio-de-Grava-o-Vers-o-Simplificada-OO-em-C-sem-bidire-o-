8ae0ab5de6ed6522ca4c2f5bc986d24b1bba6a5a
Magagnin <magagningu@gmail.com>
2025-10-27 23:52:21 -0300

    Update Musician.cs

--- src/EstudioGravacao.Domain/Entities/Musician.cs
+++ src/EstudioGravacao.Domain/Entities/Musician.cs
@@ -1,1 +1,26 @@
+using EstudioGravacao.Domain.Exceptions;
+using EstudioGravacao.Domain.ValueObjects;
 
+namespace EstudioGravacao.Domain.Entities
+{
+    public class Musician
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+        
+        // Multiplicidade 0..1 (Um músico PODE ter uma carteira sindical)
+        public UnionCard? Card { get; }
+
+        public Musician(string name, UnionCard? card = null)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new DomainException("Musician name cannot be empty.");
+            }
+
+            Id = Guid.NewGuid();
+            Name = name;
+            Card = card;
+        }
+    }
+}
